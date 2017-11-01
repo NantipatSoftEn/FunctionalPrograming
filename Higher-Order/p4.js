93666a400c2aac4ffc0d8930b0f8aa3451aaca83
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-11-01 11:43:54 +0700

    re line

--- Higher-Order/p4.js
+++ Higher-Order/p4.js
@@ -4,17 +4,14 @@ const myCart = [
  {name:'Magnum',unitPrice:45,quantity:3}
 ]
 
-
 const subtotalOfItem = item => item.unitPrice*item.quantity
 const quantityOfItem = item => item.quantity
-
 
 const sumOfCartBy = f => cart =>
       cart.map(f).reduce((x,y) => x+y,0)
 
 const subtotalOfCart = sumOfCartBy(subtotalOfItem)
 const numberOfItemsInCart =  sumOfCartBy(quantityOfItem)
-
 
 print(subtotalOfCart(myCart))
 print(numberOfItemsInCart(myCart))
