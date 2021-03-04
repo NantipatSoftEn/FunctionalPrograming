f5b18f22c54980134fc13ad494e2b771488d178f
NantipatSoften <keymasterviriya1150@gmail.com>
2021-03-04 10:19:04 +0700

    🎨   format of the code

--- typescript/play.Create-Generic-class.ts
+++ typescript/play.Create-Generic-class.ts
@@ -1,35 +1,44 @@
-class Animal {
+class Animal<T> {
+  legs: T;
+  constructor(legs: T) {
+    this.legs = legs;
+  }
   move(distanceInMeters: number = 0) {
     return `Animal moved ${distanceInMeters}m.`;
   }
 }
 
-class Dog extends Animal {
+class Dog extends Animal<number> {
+  constructor(legs: number) {
+    super(legs);
+  }
   bark() {
     return "Woof Woof";
   }
 }
 
-class Cat extends Animal {
+class Cat extends Animal<number> {
   meaw() {
     return "meaw";
   }
 }
-const dog = new Dog();
+// const dog = new Dog(4);
 
 // dog.bark();
 // dog.move(10);
 // dog.bark();
 
-function create<T extends Dog>(c: { new (): T }): T {
+function create<T extends Animal<number>>(c: { new (): T }): T {
   return new c();
 }
 
 // ลองแบบ arrow function
 
-const create2 = <T extends Dog>(c: { new (): T }) => new c();
+const create2 = <T extends Animal<number>>(c: { new (): T }) => new c();
 
+// not work ?
 const dogs = create(Dog);
-const dog2 = create2(Dog);
-console.log(dogs.bark());
-console.log(dog2.bark());
+
+// const dog2 = create2(Dog);
+// console.log(dogs.bark());
+// console.log(dog2.bark());
